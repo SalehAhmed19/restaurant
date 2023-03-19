fb62c45d9d095af911a0ed069161ba47423ad3b3
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-03-19 22:12:03 +0600

    jwt token verification and redirect to login page

--- src/Hooks/useCarts.jsx
+++ src/Hooks/useCarts.jsx
@@ -1,21 +1,48 @@
+import axios from "axios";
+import { signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../firebase.init";
 
 const useCarts = () => {
   const [carts, setCarts] = useState([]);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    if (user) {
-      fetch(`http://localhost:4000/api/cart?customerEmail=${user?.email}`, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => setCarts(data));
-    }
-  }, [user, carts]);
+    // if (user) {
+    //   try {
+    //     fetch(`http://localhost:4000/api/cart?customerEmail=${user?.email}`, {
+    //       headers: {
+    //         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    //       },
+    //     })
+    //       .then((res) => res.json())
+    //       .then((data) => setCarts(data));
+    //   } catch (error) {
+    //     console.log(error.message);
+    //   }
+    // }
+    const getCart = async () => {
+      const url = `http://localhost:4000/api/cart?customerEmail=${user?.email}`;
+      try {
+        const { data } = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        setCarts(data);
+      } catch (error) {
+        console.log(error.response.status);
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          navigate("/login");
+        }
+      }
+    };
+    getCart();
+  }, [user, carts, navigate]);
   return [carts, setCarts];
 };
 
